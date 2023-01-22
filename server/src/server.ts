a19e01f21412400f02e3cb849bd94c8eaebdbefe
Marcio Alves <marcioalvesjun@gmail.com>
2023-01-22 18:08:31 -0300

    created backend routes and started the mobile app

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,16 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Beber'
-      }
-    }
+app
+  .listen({
+    port: 3333
   })
-
-  return habits
-})
-
-app.listen({
-  port: 3333
-})
+  .then(() => {
+    console.log('HTTP Server running')
+  })
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import { Habits } from './components/Habits'
+// import { Habits } from './components/Habits'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
 
-function App() {
+export function App() {
   return (
-    <>
-      <Habits completed={10} />
-      <Habits completed={20} />
-      <Habits completed={30} />
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
-
-export default App
